375b5bac9b3582f5019424a60674443accc20922
chelsea dowling <chelseabelle.dowling@gmail.com>
2017-03-20 03:08:13 -0400

    further commented

--- tarot.js
+++ tarot.js
@@ -44,8 +44,12 @@ console.log('script loaded')
 //I chose a counter because it would help log how many times the card deck was clicked 
  let counter = -1;
 //This was incredibly frusterating because I set it at zero because as we know arrays
-//go 0,1,2... but for some reason It kept breaking, but I kid you not tried -1 and like magic it worked
-//because it counts -1 before getting to 0 which would be the 1st item. 
+//go 0,1,2... but for some reason It kept breaking, but i called taj over and 
+//we were fiddingling around with it and i randomly tried -1 as a joke and it happened to 
+//work and I have no idea why ive been googling my brains out the closest theory 
+//i came to which i pushed earlier is
+//because maybe it counts -1 before getting to 0 which would be the 1st item. 
+//so maybe it goes -1 ...and prints 0, 1, 2 
  $( ".bOc" ).on("click", function() {
  	counter++
  //here we are refrensing the deck of the back of cards which is what I want people to click 
@@ -58,6 +62,11 @@ console.log('script loaded')
 //here we have set the variable randomSrc to = the array plus the method of
 //randomization floor rounds the number down to the nearest integer which 
 //we then times that by the random method times the length of the array 
+//i got really really confused as to why this orginally wasnt working without the 1 
+//added at the end but i did some research and found this resource 
+//https://blog.kevinchisholm.com/javascript/javascript-array-length-always-one-higher/
+//essentially saying when you use the length attribute it starts at 1 not 0 
+// this still confuses me with the negative 1 above. 
 
 
 let card = $('.card');
